38d97d34d2c1036c2fb0116106fe1aa7827651c5
jetcouper <ant7945@gmail.com>
2026-03-10 17:34:01 -0400

    asdf

--- vampire-survivor/Joueur.cs
+++ vampire-survivor/Joueur.cs
@@ -33,26 +33,6 @@ public partial class Joueur : Node2D
         _camera = GetNode<Camera2D>("Camera2D");
         _camera.MakeCurrent();
         _camera.Zoom = new Vector2(CameraZoom, CameraZoom);
-
-        // var tileMap = GetParent().GetNode<TileMapLayer>("TileMapLayer");
-        // var usedRect = tileMap.GetUsedRect();
-        // var tileSize = tileMap.TileSet.TileSize;
-
-        // float mapWidth = usedRect.Size.X * tileSize.X;
-        // float mapHeight = usedRect.Size.Y * tileSize.Y;
-
-        // Vector2 screenSize = GetViewport().GetVisibleRect().Size;
-
-        // float zoomX = screenSize.X / mapWidth;
-        // float zoomY = screenSize.Y / mapHeight;
-        // float zoom = Mathf.Min(zoomX, zoomY);
-        // GD.Print($"ScreenSize: {screenSize}");
-        // GD.Print($"ZoomX: {zoomX}, ZoomY: {zoomY}, Zoom final: {zoom}");
-        // GD.Print($"Map Rect: {usedRect}");
-        // GD.Print($"Tile Size: {tileMap.TileSet.TileSize}");
-        // GD.Print($"Map pixels W: {usedRect.Size.X * tileMap.TileSet.TileSize.X}");
-        // GD.Print($"Map pixels H: {usedRect.Size.Y * tileMap.TileSet.TileSize.Y}");
-        //camera.Zoom = new Vector2(zoom, zoom);
     }
 
     public override void _Process(double delta)
---
+++ vampire-survivor/Niveau.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+
+public partial class Niveau : Node2D
+{
+    public override void _Ready() { }
+}
