064f0f31273d1d9ac036586ef1973ca5d6f86322
jetcouper <ant7945@gmail.com>
2026-03-10 18:03:55 -0400

    Pause Fait

--- vampire-survivor/Niveau.cs
+++ vampire-survivor/Niveau.cs
@@ -4,5 +4,29 @@ using Godot;
 
 public partial class Niveau : Node2D
 {
-    public override void _Ready() { }
+    private bool _enPause = true;
+
+    public override void _Ready()
+    {
+        SetChildrenPaused(true);
+        GD.Print("Niveau prêt");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (_enPause && @event.IsActionPressed("ui_select"))
+        {
+            _enPause = false;
+            SetChildrenPaused(false);
+            GD.Print("Niveau démarré");
+        }
+    }
+
+    private void SetChildrenPaused(bool paused)
+    {
+        foreach (Node child in GetChildren())
+        {
+            child.ProcessMode = paused ? ProcessModeEnum.Disabled : ProcessModeEnum.Inherit;
+        }
+    }
 }
