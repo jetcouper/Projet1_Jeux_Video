05a518cb7e109b713c5ca918fca9de8e3213cd6d
jetcouper <ant7945@gmail.com>
2026-03-13 15:05:42 -0400

    Spikes

--- vampire-survivor/src/Weapons/DPM/DpmMovementBullet.cs
+++ vampire-survivor/src/Weapons/DPM/DpmMovementBullet.cs
@@ -3,47 +3,47 @@ using Godot;
 
 public partial class DpmMovementBullet : Node2D
 {
-	[ExportGroup("External")]
-	[Export]
-	public Node2D NodeToControl;
+    [ExportGroup("External")]
+    [Export]
+    public Node2D NodeToControl;
 
-	public Node2D Player;
+    public Node2D Player;
 
-	public float DistFromPlayer = 50f;
-	
-	public float AngularSpeed;
+    public float DistFromPlayer = 50f;
 
-	private float _speed = 50f;
+    public float AngularSpeed;
 
-	public float startingPosition; 
+    private float _speed = 50f;
 
-	private Vector2 _originPosition;
+    public float startingPosition;
 
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
-		MoveBullet(delta);
-	}
+    private Vector2 _originPosition;
 
-	public void StartSwing()
-	{
-		_originPosition = Player.GlobalPosition;
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        MoveBullet(delta);
+    }
+
+    public void StartSwing()
+    {
+        _originPosition = Player.GlobalPosition;
         Vector2 offset = new Vector2(Mathf.Cos(startingPosition), Mathf.Sin(startingPosition));
         NodeToControl.GlobalPosition = _originPosition + offset * DistFromPlayer;
 
         NodeToControl.Rotation = startingPosition + 55;
-	}
+    }
 
-	private void MoveBullet(double delta)
-	{
-		startingPosition += AngularSpeed * (float)delta; 
-		DistFromPlayer += _speed * (float)delta;
+    private void MoveBullet(double delta)
+    {
+        startingPosition += AngularSpeed * (float)delta;
+        DistFromPlayer += _speed * (float)delta;
 
-		
-		// Move bullet straight along its angle
-		Vector2 offset = new Vector2(Mathf.Cos(startingPosition), Mathf.Sin(startingPosition)) * DistFromPlayer;
-		NodeToControl.GlobalPosition = _originPosition + offset;
+        // Move bullet straight along its angle
+        Vector2 offset =
+            new Vector2(Mathf.Cos(startingPosition), Mathf.Sin(startingPosition)) * DistFromPlayer;
+        NodeToControl.GlobalPosition = _originPosition + offset;
 
-		NodeToControl.Rotation = startingPosition + 55;
-	}
+        NodeToControl.Rotation = startingPosition + 55;
+    }
 }
--- vampire-survivor/src/Weapons/Useable/Sword.cs
+++ vampire-survivor/src/Weapons/Useable/Sword.cs
@@ -3,17 +3,17 @@ using Godot;
 
 public partial class Sword : Node2D, IUseable
 {
-	[ExportGroup("Internal")]
-	[Export]
-	private DpmMovementSword DpmMovementSword;
+    [ExportGroup("Internal")]
+    [Export]
+    private DpmMovementSword DpmMovementSword;
 
-	public void Use()
-	{
-		DpmMovementSword.StartSwing();
-	}
+    public void Use()
+    {
+        DpmMovementSword.StartSwing();
+    }
 
-	public void setPlayer(Node2D player)
-	{
-		DpmMovementSword.Player = player;
-	}
+    public void setPlayer(Node2D player)
+    {
+        DpmMovementSword.Player = player;
+    }
 }
