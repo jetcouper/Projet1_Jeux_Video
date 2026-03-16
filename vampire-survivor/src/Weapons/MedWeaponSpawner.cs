35b8e9f64abdfd0fc0065f0956cfeb1bbbd39a7f
Charlee <cha.cyr.d@gmail.com>
2026-03-16 11:29:50 -0400

    niveau

--- vampire-survivor/src/Weapons/MedWeaponSpawner.cs
+++ vampire-survivor/src/Weapons/MedWeaponSpawner.cs
@@ -6,79 +6,79 @@ using static MedPositions;
 
 public partial class MedWeaponSpawner : Node2D
 {
-    [ExportGroup("Internal")]
-    [Export]
-    private DcmSimpleWeaponSpawner SpawnerSword;
+	[ExportGroup("Internal")]
+	[Export]
+	private DcmSimpleWeaponSpawner SpawnerSword;
 
-    [Export]
-    private DcmSimpleWeaponSpawner SpawnerAxe;
+	[Export]
+	private DcmSimpleWeaponSpawner SpawnerAxe;
 
-    [Export]
-    private DcmBulletSpawner BulletSpawner;
+	[Export]
+	private DcmBulletSpawner BulletSpawner;
 
-    [Export]
-    public EAlgoSelectionCible InAlgoSelectionWeapon;
+	[Export]
+	public EAlgoSelectionCible InAlgoSelectionWeapon;
 
-    [Export]
-    public MedPositions MedPositions;
+	[Export]
+	public MedPositions MedPositions;
 
-    public enum EAlgoSelectionCible
-    {
-        eSword,
-        eAxe,
-        eLinearSprayBullets,
-        eCircularSprayBullets,
-        eLinearSpiralBullets,
-        eCircularSpiralBullets,
-    }
+	public enum EAlgoSelectionCible
+	{
+		eSword,
+		eAxe,
+		eLinearSprayBullets,
+		eCircularSprayBullets,
+		eLinearSpiralBullets,
+		eCircularSpiralBullets,
+	}
 
-    public override void _Ready()
-    {
-        base._Ready();
-        Spawn();
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		Spawn();
+	}
 
-    public void Spawn()
-    {
-        switch (InAlgoSelectionWeapon)
-        {
-            case EAlgoSelectionCible.eSword:
-                {
-                    SpawnerSword.Spawn(0);
-                }
-                break;
-            case EAlgoSelectionCible.eAxe:
-                {
-                    SpawnerAxe.Spawn(0);
-                }
-                break;
-            case EAlgoSelectionCible.eLinearSprayBullets:
-                {
-                    BulletSpawner.Spawn(0);
-                }
-                break;
-            case EAlgoSelectionCible.eCircularSprayBullets:
-                {
-                    BulletSpawner.Spawn(1);
-                }
-                break;
-            case EAlgoSelectionCible.eLinearSpiralBullets:
-                {
-                    BulletSpawner.SpawnSequential(0);
-                }
-                break;
-            case EAlgoSelectionCible.eCircularSpiralBullets:
-                {
-                    BulletSpawner.SpawnSequential(1);
-                }
-                break;
-            default:
-                break;
-        }
-    }
+	public void Spawn()
+	{
+		switch (InAlgoSelectionWeapon)
+		{
+			case EAlgoSelectionCible.eSword:
+				{
+					SpawnerSword.Spawn(0);
+				}
+				break;
+			case EAlgoSelectionCible.eAxe:
+				{
+					SpawnerAxe.Spawn(0);
+				}
+				break;
+			case EAlgoSelectionCible.eLinearSprayBullets:
+				{
+					BulletSpawner.Spawn(0);
+				}
+				break;
+			case EAlgoSelectionCible.eCircularSprayBullets:
+				{
+					BulletSpawner.Spawn(1);
+				}
+				break;
+			case EAlgoSelectionCible.eLinearSpiralBullets:
+				{
+					BulletSpawner.SpawnSequential(0);
+				}
+				break;
+			case EAlgoSelectionCible.eCircularSpiralBullets:
+				{
+					BulletSpawner.SpawnSequential(1);
+				}
+				break;
+			default:
+				break;
+		}
+	}
 
-    public Node2D GetPlayer()
-    {
-        return MedPositions.choisirObjet(EAlgoSelectionObjet.ePlayer, new(0, 0));
-    }
+	public Node2D GetPlayer()
+	{
+		return MedPositions.choisirObjet(EAlgoSelectionObjet.ePlayer, new(0, 0));
+	}
 }
